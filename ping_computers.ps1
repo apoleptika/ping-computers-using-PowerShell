82b0fad55da165bdcd7784b3bed01340621f96d1
Abdurrahim Hacıoğlu <apoleptika@hotmail.com>
2022-10-11 14:12:57 +0300

    Update ping_computers.ps1

--- ping_computers.ps1
+++ ping_computers.ps1
@@ -1,3 +1,4 @@
+
 # SystemIPs.txt, this file includes computer IP address or computer/system names.
 # 192.168.30.31
 # DC01
@@ -8,22 +9,24 @@
 
 # clear the screen
 Clear 
-$Systems = Get-Content “SystemIPs.txt”
+$Systems = Get-Content "SystemIPs.txt"
 # To convert Hard Disk info to GB
 $ConvertToGB = (1024 * 1024 * 1024)  
 foreach ($sys in $Systems)
 {
 if ( Test-Connection $sys -Count 1 -ErrorAction SilentlyContinue )
     {    
-        $FreeMemory=(systeminfo /S $sys  | Select-String ‘Available Physical Memory:’).ToString().Split(‘:’)[1].Trim()
-        $SystemName=(systeminfo /S $sys  | Select-String ‘OS Name:’).ToString().Split(‘:’)[1].Trim()
-        $SystemBootTime=[datetime]::ParseExact($(systeminfo /S $sys | find “System Boot Time:”).substring(27,20),”dd/MM/yyyy, HH:mm:ss”,$null)
-        $Disk = Get-WmiObject Win32_LogicalDisk -ComputerName $sys -Filter “DeviceID=’C:'” | Select-Object Size,FreeSpace
-        $Disk= “{0:N1}” -f ($Disk.Size / $ConvertToGB) + “gb”
-        Write-Host $sys “Sistem OK “, $SystemName ,”FreeRam:” $FreeMemory, “C: FreeDisk”  $Disk, “BootTime:” $SystemBootTime  -ForegroundColor DarkGreen
+        $FreeMemory=(systeminfo /S $sys  | Select-String 'Available Physical Memory:').ToString().Split(':')[1].Trim()
+        $SystemName=(systeminfo /S $sys  | Select-String 'OS Name:').ToString().Split(':')[1].Trim()
+        $SystemBootTime=[datetime]::ParseExact($(systeminfo /S $sys | find "System Boot Time:").substring(27,20),"dd/MM/yyyy, HH:mm:ss",$null)
+        $Disk = Get-WmiObject Win32_LogicalDisk -ComputerName $sys -Filter "DeviceID='C:'" | Select-Object Size,FreeSpace
+        $Disk= "{0:N1}" -f ($Disk.Size / $ConvertToGB) + "gb"
+        Write-Host $sys "Sistem OK ", $SystemName ,"FreeRam:" $FreeMemory, "C: FreeDisk"  $Disk, "BootTime:" $SystemBootTime  -ForegroundColor DarkGreen
     }
 else
     {
-        Write-Host “There is Problem ???” $sys -ForegroundColor DarkRed
+        Write-Host "There is problem in this copmuter/system : " $sys -ForegroundColor DarkRed
     }
 }
+
+
